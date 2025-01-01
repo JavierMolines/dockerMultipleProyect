54b61badb6b8b9288651694d17b7d73dd20338da
JavierMolines <eljavimolines@gmail.com>
2025-01-01 13:22:17 -0300

    feat: upload dependencies

--- server/src/app/fetch.ts
+++ server/src/app/fetch.ts
@@ -3,13 +3,15 @@ import https from "node:https";
 
 const fetch = async (url: string) => {
 	const executorPromise = (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 		resolve: (value?: any) => void,
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 		reject: (reason?: any) => void,
 	) => {
 		const handlerStream = (response: IncomingMessage) => {
 			let data = "";
 
-			response.on("data", (chunk: any) => {
+			response.on("data", (chunk) => {
 				data += chunk;
 			});
 
